eaac6d6d61871f9eb8a3ac7ecbfa08b5f3dd1c8b
Nazia Shehnaz <59027621+Geek-a-Byte@users.noreply.github.com>
2020-11-22 09:50:15 +0600

    Doctor's Signin Added

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@
   const patientrole= document.getElementById('patient-role');
   const contactno= document.getElementById('phone');
   const address= document.getElementById('address');
+  const Signinrole=document.getElementById('Signinrole');
+  const Signinemail=document.getElementById('Signinemail_id');
+  const Signinpassword=document.getElementById('Signinpassword');
   const database =firebase.database();
 
   addUserBtn.addEventListener('click',(e)=>{
@@ -40,10 +43,98 @@
            username: patientusername.value,           
            role:patientrole.value,           
            password: patientpassword.value,
+           
+           
         
 
          });
          
        window.location.reload()
+        
+  });
+
+//   var leadsRef = database.ref('');
+// leadsRef.on('value', function(snapshot) {
+//     snapshot.forEach(function(childSnapshot) {
+//       var childData = childSnapshot.val();
+//     });
+// });
+
+
+// var userId = firebase.auth().currentUser.uid;
+// return firebase.database().ref('/Doctors/' + username.value).once('value').then((snapshot) => {
+//   var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+//   // ...
+// });
+
+        //get comments whose frompage equals this page's pathname
+
+       
+        SigninBtn.addEventListener('click',(e)=>{
+         e.preventDefault();
+            var DoctorRef=firebase.database().ref('Doctors/');
+            var PatientRef=firebase.database().ref('Patients/');
+        DoctorRef.once('value',function(snapshot){
+                   snapshot.forEach(function(itemSnapshot){
+                        //get the object for one snapshot
+                        var itemData=itemSnapshot.val();
+                        var comment=itemData.comment ;
+                        // var name=itemData. username;
+                        var email=itemData. email;
+                        var password=itemData. password;
+                        var role=itemData. role;
+                        
+                        if(email==Signinemail.value && password==Signinpassword.value && role==Signinrole.value)
+                        {
+                          alert("User Verified");
+                          //  window.location.reload();
+                           if(role=="Doctor")
+                           {
+                              window. location. replace("../Doctor/DocPortal.html");
+                           }
+                           else if(role=="Patient")
+                           {
+                              window. location. replace("../Patient/PatientPortal.html");
+                           }
+                         
+                        }
+                        // else
+                        // {
+                        //   alert("User Not Verified");
+                        //   console.log("user not found")
+                        // }
+
+                        
+                   })
+        })
+       PatientRef.once('value',function(snapshot){
+                   snapshot.forEach(function(itemSnapshot){
+                        //get the object for one snapshot
+                        var itemData=itemSnapshot.val();
+                        var password=itemData.password;
+                        var role=itemData.role;
+                        
+                        if( password==Signinpassword.value && role==Signinrole.value)
+                        {
+                          alert("User Verified");
+                          //  window.location.reload();
+                        
+                           if(role=="Patient")
+                           {
+                              window. location. replace("../Patient/PatientPortal.html");
+                           }
+                         
+                        }
+                        // else
+                        // {
+                        //   alert("User Not Verified");
+                        //   console.log("user not found")
+                        // }
+
+                        
+                   })
+        })
+         
+      
         
   });
